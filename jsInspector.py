9ee5ad6bcb86606b48a68793dce11e73874fc3ea
Ben Dragisic <benjamin.dragisic@pentestpeople.com>
2023-07-20 11:48:41 +0100

    updated ui

--- jsInspector.py
+++ jsInspector.py
@@ -8,41 +8,51 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Common 2D array of JS packages and the console commands to reveal their version
+# 2D array of common JS packages and the console commands to reveal their version
 libraries = [
-    ["Adobe Target", "adobe.target.VERSION"],
-    ["Angular", "getAllAngularRootElements()[0].attributes['ng-version']"],
-    ["AngularJS", "angular.version.full"],
-    ["Bootstrap", "bootstrap.Alert.VERSION "],
-    ["Bootstrap", "$.fn.tooltip.Constructor.VERSION"],
-    ["Core", "core.version"],
-    ["Core", "window['__core-js_shared__'].versions[0].version"],
-    ["D3", "d3.version"],
-    ["Data Tables", "$.fn.dataTable.version"],
-    ["Data Tables", "$().dataTable.version"],
-    ["Dojo", "dojo.version"],
-    ["Dropzone", "Dropzone.version"],
-    ["Ember.js", "Ember.VERSION"],
-    ["ExtJS 3.x", "Ext.version"],
-    ["ExtJS 4.0", "Ext.getVersion('extjs')"],
-    ["ExtJS >= 4.1", "Ext.getVersion().version"],
-    ["FancyBox", "jQuery.fancybox.version"],
-    ["Highcharts", "Highcharts.version"],
-    ["jQuery", "jQuery().jquery"],
-    ["jQueryUI", "jQuery.ui.version"],
-    ['jQueryUI', "$.ui.version"],
-    ["Knockout.js", "ko.version"],
-    ["Lodash", "_.VERSION"],
-    ["Microsoft Clarity", "clarity.v"],
-    ["Migrate", "jQuery.migrateVersion"],
-    ["Modernizr", "Modernizr._version"],
-    ["Moment", "moment.version"],
-    ["Prototype", "prototype.version"],
-    ["React", "React.version"],
-    ["RequireJS", "require.version"],
-    ["Toastr", "toastr.version"],
-    ["Wordpress Emoji", "window._wpemojiSettings.source.concatemoji"],
-    ["YUI", "Y.VERSION"]
+    # ["Adobe Target", "adobe.target.VERSION", ''],
+    # ["Angular", "getAllAngularRootElements()[0].attributes['ng-version']",
+    #  'https://code.angularjs.org/'],
+    # ["AngularJS", "angular.version.full", 'https://code.angularjs.org/'],
+    # ["Bootstrap", "bootstrap.Alert.VERSION ",
+    #     'https://getbootstrap.com/docs/versions/'],
+    # ["Bootstrap", "$.fn.tooltip.Constructor.VERSION",
+    #     'https://getbootstrap.com/docs/versions/'],
+    ["Core", "core.version", 'https://www.npmjs.com/package/core-js'],
+    ["Core", "window['__core-js_shared__'].versions[0].version",
+        'https://www.npmjs.com/package/core-js'],
+    ["D3", "d3.version", 'https://github.com/d3/d3/releases'],
+    ["Data Tables", "$.fn.dataTable.version",
+        'https://cdn.datatables.net/releases.html'],
+    ["Data Tables", "$().dataTable.version",
+     'https://cdn.datatables.net/releases.html'],
+    ["Dojo", "dojo.version", 'https://github.com/dojo/dojo'],
+    ["Dropzone", "Dropzone.version", 'https://www.npmjs.com/package/dropzone'],
+    ["Ember.js", "Ember.VERSION", 'https://emberjs.com/releases/'],
+    ["ExtJS 3.x", "Ext.version", 'https://www.sencha.com/products/extjs/'],
+    ["ExtJS 4.0", "Ext.getVersion('extjs')",
+     'https://www.sencha.com/products/extjs/'],
+    ["ExtJS >= 4.1", "Ext.getVersion().version",
+     'https://www.sencha.com/products/extjs/'],
+    ["FancyBox", "jQuery.fancybox.version", 'http://fancybox.net/'],
+    ["Highcharts", "Highcharts.version",
+        'https://www.npmjs.com/package/highcharts?activeTab=versions'],
+    ["jQuery", "jQuery().jquery", 'https://releases.jquery.com/jquery/'],
+    ["jQueryUI", "jQuery.ui.version", 'https://releases.jquery.com/ui/'],
+    ['jQueryUI', "$.ui.version", 'https://releases.jquery.com/ui/'],
+    ["Knockout.js", "ko.version", 'https://knockoutjs.com/downloads/'],
+    ["Lodash", "_.VERSION", 'https://www.npmjs.com/package/lodash?activeTab=versions'],
+    ["Microsoft Clarity", "clarity.v", 'https://www.npmjs.com/package/clarity-js'],
+    ["Migrate", "jQuery.migrateVersion", 'https://releases.jquery.com/jquery/'],
+    ["Modernizr", "Modernizr._version", 'https://www.npmjs.com/package/modernizr'],
+    ["Moment", "moment.version", 'https://www.npmjs.com/package/moment'],
+    ["Prototype", "prototype.version", 'http://prototypejs.org/download/'],
+    ["React", "React.version", 'https://github.com/facebook/react/releases'],
+    ["RequireJS", "require.version", 'https://requirejs.org/docs/download.html'],
+    ["Toastr", "toastr.version", 'https://www.npmjs.com/package/toastr'],
+    ["Wordpress Emoji", "window._wpemojiSettings.source.concatemoji",
+        'https://wordpress.org/support/topic/wp-emoji-release-min-js/'],
+    ["YUI", "Y.VERSION", 'https://clarle.github.io/yui3/']
 ]
 
 
@@ -95,6 +105,8 @@ Developed by github.com/BDragisic
     target = args.target if 'https' in args.target else 'https://'+args.target
     print('\nTarget:', colored(f'{target}\n', 'blue'))
     for package in libraries:
+        print(
+            f'    [*] Checking {package[0]}                                       ', end='\r')
         command = package[1]
         try:
             versionDetected = get_version(target, command)
@@ -106,5 +118,8 @@ Developed by github.com/BDragisic
 
         if versionDetected:
             # driver.execute_script() returns None if the commands returned nothing
-            print(f'    [*] {package[0]}:',
-                  colored(f'{versionDetected}', 'green'))
+            print(f'\n    [*] {package[0]}:')
+            print('        [*] Version detected:',
+                  colored(versionDetected, 'green'))
+            print('        [*] Latest release:',
+                  colored(package[2], 'green'))
